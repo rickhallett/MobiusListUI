688f5c42c3ac31c99e46fa116e95aa01c5030d4d
Rick Hallett <rickhallett@live.co.uk>
2020-01-26 12:57:57 +0000

    createProduct with user notifications

--- src/pages/Products.vue
+++ src/pages/Products.vue
@@ -16,8 +16,8 @@
             <q-select dense label="Category" :options="categories" v-model="newProduct.categoryId"></q-select>
           </q-card-section>
           <q-card-section class="row justify-end">
-            <q-btn flat size="20px" icon="backup" color="secondary" class="submit-button"></q-btn>
-            <q-btn flat size="20px" icon="cancel" color="negative" class="submit-button"></q-btn>
+            <q-btn flat size="20px" icon="backup" color="secondary" class="submit-button" @click="createProduct"></q-btn>
+            <q-btn flat size="20px" icon="cancel" color="negative" class="submit-button" @click="clearProduct"></q-btn>
           </q-card-section>
         </q-card>
       </div>
@@ -29,7 +29,6 @@
               <q-icon name="add" size="sm" color="secondary"></q-icon>
             </div>
           </q-card-section>
-
           <q-card-section class="q-gutter-y-md">
             <q-input dense label="Name" placeholer="Name" v-model="newCategory.name"></q-input>
           </q-card-section>
@@ -49,19 +48,18 @@
               title="Products"
               :data="products"
               :columns="productColumns"
-              row-key="name"
+              row-key="productNumber"
               table-class="text-grey-8"
               table-style="padding: 0 40px"
+              :loading="tableLoading"
           >
             <template slot="top">
-              <q-select dense label="Filter Category" style="width: 200px"></q-select>
+              <q-select dense label="Filter Category" style="width: 200px" :options="categories" v-model="productFilter" clearable></q-select>
             </template>
           </q-table>
         </q-card>
       </div>
     </div>
-
-
 
   </q-page>
 </template>
@@ -71,6 +69,7 @@
         name: "Products",
         data() {
             return {
+                tableLoading: false,
                 newProduct: {
                     name: null,
                     description: null,
@@ -80,50 +79,121 @@
                 newCategory: {
                     name: null
                 },
-                categories: [
-                    {
-                        label: 'Technology',
-                        value: 1
-                    },
-                    {
-                        label: 'Peripheral',
-                        value: 1
-                    },
-                ],
+                categories: [],
                 products: [],
                 productColumns: [
+                    { name: 'productNumber', label: 'Product Number', field: 'productNumber', align: 'left', sortable: true },
                     { name: 'name', label: 'Name', field: 'name', align: 'left', sortable: true },
                     { name: 'description', label: 'Description', field: 'description', align: 'left', sortable: false },
                     { name: 'price', label: 'Price', field: 'price', sortable: false }
-                ]
+                ],
+                productFilter: null
+            }
+        },
+        watch: {
+            productFilter(val) {
+                if (val) {
+                    this.getAllProductsByCategory(val.label);
+                    return;
+                }
+
+                this.getAllProducts();
+            }
+        },
+        methods: {
+            createProduct() {
+                console.log('newProduct1', this.newProduct);
+                const newProduct = {
+                    name: this.newProduct.name,
+                    description: this.newProduct.description,
+                    price: Number.parseFloat(this.newProduct.price),
+                    categoryId: Number.parseInt(this.newProduct.categoryId.value)
+                };
+                console.log('newProduct', newProduct);
+                this.$axios.post('https://localhost:5001/api/v1/products', newProduct)
+                    .then(res => {
+                        console.log(res);
+                        if (res.status === 201) {
+                            this.$q.notify({
+                                message: `Product Number: ${res.data.productNumber} created`,
+                                color: 'positive',
+                                classes: 'notification'
+                            })
+                        }
+                    })
+                    .catch(err => {
+                        console.log(err.response);
+                        if (err.response.data.title === "One or more validation errors occurred.") {
+                            this.$q.notify({
+                                message: 'One or more validation errors occurred',
+                                color: 'warning',
+                                classes: 'notification'
+                            })
+                        }
+                    })
+            },
+            clearProduct() {
+                this.newProduct = {
+                    name: null,
+                    description: null,
+                    price: null,
+                    categoryId: null
+                }
+            },
+            getAllProductsByCategory(name) {
+                this.tableLoading = true;
+                this.$axios.get(`https://localhost:5001/api/v1/products?category=${name}`)
+                    .then(res => {
+                        this.products = res.data;
+                        this.tableLoading = false;
+                    })
+                    .catch(err => {
+                        console.log(err);
+                        this.tableLoading = false;
+                        this.createServerErrorMessage();
+                    });
+            },
+            getAllProducts() {
+                this.tableLoading = true;
+                this.$axios.get('https://localhost:5001/api/v1/products')
+                    .then(res => {
+                        this.tableLoading = false;
+                        this.products = res.data;
+                        console.log(this.products)
+                    })
+                    .catch(err => {
+                        console.log(err);
+                        this.tableLoading = false;
+                        this.createServerErrorMessage();
+                    });
+            },
+            getAllCategories() {
+                this.$axios.get('https://localhost:5001/api/v1/category')
+                    .then(res => {
+                        this.categories = res.data.map(cat => {
+                            return {
+                                label: cat.name,
+                                value: cat.id
+                            }
+                        })
+                    })
+                    .catch(err => {
+                        console.log(err);
+                        this.tableLoading = false;
+                        this.createServerErrorMessage();
+                    });
+            },
+            createServerErrorMessage() {
+                this.$q.notify({
+                    message: 'Error contacting server',
+                    color: 'negative',
+                    classes: 'notification'
+                })
             }
         },
         created() {
-            this.$axios.get('https://localhost:5001/api/v1/products')
-                .then(res => {
-                    this.products = res.data;
-                })
-                .catch(err => {
-                    console.log(err);
-                    this.$q.notify({
-                        message: 'Error contacting server',
-                        color: 'negative',
-                        classes: 'notification'
-                    })
-                });
-
-            this.$axios.get('https://localhost:5001/api/v1/category')
-                .then(res => {
-                    this.categories = res.data.map(cat => {
-                        return {
-                            label: cat.name,
-                            value: cat.id
-                        }
-                    })
-                })
-                .catch(err => {
-
-                });
+            this.getAllProducts();
+            this.getAllCategories();
         }
     }
 </script>
